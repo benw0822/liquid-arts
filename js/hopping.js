c7ed204f6666c311ae9b0284a128274f28d2657b
benw0822 <benw822@gmail.com>
2025-12-13 17:15:23 +0800

    Update hopping.js

--- js/hopping.js
+++ js/hopping.js
@@ -1233,7 +1233,7 @@ window.getHopCardHTML = function (hop, user, comments = [], barInfo, cheersInfo 
                         <span id="cheers-count-${hopId}">${cheersCount}</span>
                     </button>
                     <button class="btn-interaction" onclick="window.toggleCardPanel('${hopId}')">
-                        <svg class="interaction-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2-2z"></path></svg>
+                        <svg class="interaction-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
                     </button>
                  </div>
              </div>
